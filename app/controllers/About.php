30d94dfcc3f8b9fe76de6ca5ca05cb29518d5fa3
Refri Rifwan <refririfwan@gmail.com>
2019-11-26 11:34:29 +0700

    mvc view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
-class About
+class About extends Controller
 {
-    public function index($nama = "admin", $pekerjaan = "admin")
+    public function index($nama = "admin", $pekerjaan = "admin", $umur = 17)
     {
-        echo "Halo, Nama Saya $nama, Saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
